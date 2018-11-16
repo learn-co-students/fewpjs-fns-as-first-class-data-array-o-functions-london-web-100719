ef1b9db84be11ab44c47372bbe07f3a68082381a
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-16 15:47:40 -0500

    Updates tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,21 +1,27 @@
+var dogName = "Byron"
+var dogBreed = "poodle"
+
 describe("index.js", () => {
   it("has a function wakeDog", () => {
-    expect(wakeDog()).to.equal('Wake Byron the poodle')
+    expect(wakeDog()).to.equal(`Wake ${dogName} the ${dogBreed}`)
   })
   it("has a function leashDog", () => {
-    expect(leashDog()).to.equal()
+    expect(leashDog()).to.equal(`Leash ${dogName} the ${dogBreed}`)
   })
   it("has a function walkToPark", () => {
-    expect(walkToPark()).to.equal()
+    expect(walkToPark()).to.equal(`Walk to the park with ${dogName} the ${dogBreed}`)
   })
   it("has a function throwFrisbee", () => {
-    expect(throwFrisbee()).to.equal()
+    expect(throwFrisbee()).to.equal(`Throw the frisbee for ${dogName} the ${dogBreed}`)
   })
   it("has a function walkHome", () => {
-    expect(walkHome()).to.equal()
+    expect(walkHome()).to.equal(`Walk home with ${dogName} the ${dogBreed}`)
   })
   it("has a function unleashDog", () => {
-    expect(unleashDog()).to.equal()
+    expect(unleashDog()).to.equal(`Unleash ${dogName} the ${dogBreed}`)
+  })
+  it("accepts 2 parameters for dogName and dogBreed", () => {
+    expect(wakeDog("Skippy", "Welsh Corgi")).to.equal(`Wake Skippy the Welsh Corgi`)
   })
   it("has variables for each activity", () => {
     expect(wakeDog).to.exist
@@ -26,10 +32,10 @@ describe("index.js", () => {
     expect(unleashDog).to.exist
   })
   it("has an array called 'routine' that contains each activity", () => {
-    expect(functionsArray).to.be.a('array');
-    expect(functionsArray).to.include(wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog);
+    expect(routine).to.be.a('array');
+    expect(routine).to.include(wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog);
   })
   it("has a function exerciseDog that returns the full day's dog exercise routine", () => {
-    expect(functionsArray).to.have.members([monday,tuesday,wednesday,thursday,friday]);
+    expect(routine).to.have.members([wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]);
   })
 })
